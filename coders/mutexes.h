1b20bc35718dd05c6b8ca2079218f5ee2b03d9a9
Cu-chi <110144918+Cu-chi@users.noreply.github.com>
2026-04-20 11:22:26 +0200

    fix(header/mutexes): header protection

--- coders/mutexes.h
+++ coders/mutexes.h
@@ -6,11 +6,14 @@
 /*   By: equentin <equentin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/14 16:07:37 by equentin          #+#    #+#             */
-/*   Updated: 2026/04/14 16:08:16 by equentin         ###   ########.fr       */
+/*   Updated: 2026/04/20 11:21:48 by equentin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "codexion.h"
+#ifndef MUTEXES_H
+# define MUTEXES_H
+# include "codexion.h"
 
 int		init_mutexes_cond(t_data *data);
 void	destroy_mutex_cond(t_data *data, int destroy_lim, int cond);
+#endif
