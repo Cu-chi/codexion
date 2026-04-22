be8a3fcc0c56826f9da02a4c5d9784c298628f6e
Cu-chi <110144918+Cu-chi@users.noreply.github.com>
2026-04-22 14:42:10 +0200

    tweak(parsing): reject any non numerical character

--- coders/typedef.h
+++ coders/typedef.h
@@ -1,17 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   typedef.h                                             :+:      :+:    :+:   */
+/*   typedef.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: equentin <equentin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/20 18:36:55 by equentin          #+#    #+#             */
-/*   Updated: 2026/04/20 18:39:28 by equentin         ###   ########.fr       */
+/*   Created: 2026/04/22 14:41:30 by equentin          #+#    #+#             */
+/*   Updated: 2026/04/22 14:41:48 by equentin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TYPEDEF_H
 # define TYPEDEF_H
+
 typedef struct s_data	t_data;
 typedef struct s_coder	t_coder;
 typedef struct s_queue	t_queue;
--- parsing.c
+++ parsing.c
@@ -6,7 +6,7 @@
 /*   By: equentin <equentin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/06 12:48:27 by equentin          #+#    #+#             */
-/*   Updated: 2026/04/17 19:05:54 by equentin         ###   ########.fr       */
+/*   Updated: 2026/04/22 14:40:32 by equentin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,8 @@ int	validate(t_parsed *parsed)
 		|| parsed->dongle_cooldown < 0)
 	{
 		fprintf(stderr, "Invalid value(s), all values except scheduler must be "
-			"positive integer, number_of_coders and time_to_burnout must be"
-			" greater than 0\n");
+			"positive integer without sign, number_of_coders and "
+			"time_to_burnout must be greater than 0\n");
 		return (1);
 	}
 	if (strcmp(parsed->scheduler, "fifo") != 0 && strcmp(parsed->scheduler,
@@ -36,6 +36,20 @@ int	validate(t_parsed *parsed)
 		return (1);
 	}
 	return (0);
+}
+
+int	check_num(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (-1);
+		i++;
+	}
+	return (atoi(str));
 }
 
 int	parse(t_data *data, int ac, char **av)
@@ -50,13 +64,13 @@ int	parse(t_data *data, int ac, char **av)
 			av[0]);
 		return (1);
 	}
-	data->parsed.number_of_coders = atoi(av[1]);
-	data->parsed.time_to_burnout = atoi(av[2]);
-	data->parsed.time_to_compile = atoi(av[3]);
-	data->parsed.time_to_debug = atoi(av[4]);
-	data->parsed.time_to_refactor = atoi(av[5]);
-	data->parsed.number_of_compiles_required = atoi(av[6]);
-	data->parsed.dongle_cooldown = atoi(av[7]);
+	data->parsed.number_of_coders = check_num(av[1]);
+	data->parsed.time_to_burnout = check_num(av[2]);
+	data->parsed.time_to_compile = check_num(av[3]);
+	data->parsed.time_to_debug = check_num(av[4]);
+	data->parsed.time_to_refactor = check_num(av[5]);
+	data->parsed.number_of_compiles_required = check_num(av[6]);
+	data->parsed.dongle_cooldown = check_num(av[7]);
 	data->parsed.scheduler = av[8];
 	return (validate(&data->parsed));
 }
